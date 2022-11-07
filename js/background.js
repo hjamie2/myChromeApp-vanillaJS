ae4f5e248b5e5ec6063aaf06ece24e9035e01ce1
hjamie2 <hjamie00@gmail.com>
2022-11-07 10:52:22 -0600

    Add random image

--- js/background.js
+++ js/background.js
@@ -1,1 +1,8 @@
 const imagesArray = ["0.jpg", '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+
+const randomImage = imagesArray[Math.floor(Math.random() * imagesArray.length)];
+
+const imageElement = document.createElement("img");
+imageElement.src = `img/${randomImage}`;
+// console.log(imageElement);
+document.body.appendChild(imageElement);
