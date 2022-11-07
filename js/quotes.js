be4b35306e513cc84d360ff8515ebd176666d202
hjamie2 <hjamie00@gmail.com>
2022-11-06 21:16:27 -0600

    Add starting files

---
+++ js/quotes.js
@@ -0,0 +1,41 @@
+const quotes = [
+    {
+        quote: "A strong person is someone who can protect their inner kindness.",
+    },
+    {
+        quote: "Recipe for happiness: Mature defense mech, stable long-term relationships, no drinking, no smoking, exercise, staying in shape.",
+    },
+    {
+        quote: "You will know it's time to push to the next level when you start getting too comfortable.",
+    },
+    {
+        quote: "You can't reach the top without going up hill.",
+    },
+    {
+        quote: "Harder it is to get it, the longer it will stay.",
+    },
+    {
+        quote: "Success doesn't come from a secret recipe, it comes from dedication.",
+    },
+    {
+        quote: "If I lose self-confidence, the whole world becomes my enemy.",
+    },
+    {
+        quote: "Zero times anything is zero. No matter how small, fing positive meaning and add it as your life's asset one by one.",
+    },
+    {
+        quote: "You can interpret everything people say into one of two things: asking for favor (please) or showing appreciation (thank you).",
+    },
+    {
+        quote: "Knowledge + Experience = Skill => ++ Confidence",
+    },
+    {
+        quote: "When in doubt, don't leave it to your imagination. Find solution through experience.",
+    },
+    {
+        quote: "You are the average of the 5 people you spend the most time with.",
+    },
+    {
+        quote: "Your internal qualities are your most valuable assets. All your expensive clotehs can be taken away, but your own style and taste curated stays with you.",
+    },
+]
