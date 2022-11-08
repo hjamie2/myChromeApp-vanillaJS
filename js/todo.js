367c4d069a29a8f5133e365d10b89b383b2f2048
hjamie2 <hjamie00@gmail.com>
2022-11-08 10:17:58 -0600

    Save todos in local storage

--- js/todo.js
+++ js/todo.js
@@ -2,17 +2,7 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
-const onTodoFormSubmit = (event) => {
-    event.preventDefault();
-    const newTodo = todoInput.value;
-    todoInput.value = "";
-    paintTodo(newTodo);
-}
-
-const onDeleteBtnClick = (event) => {
-    const li = event.target.parentElement;
-    li.remove();
-}
+const todosArray = [];
 
 const paintTodo = (todoItem) => {
     const liElement = document.createElement("li");
@@ -23,6 +13,24 @@ const paintTodo = (todoItem) => {
     buttonElement.addEventListener("click", onDeleteBtnClick);
     liElement.append(spanElement, buttonElement);
     todoList.append(liElement);
+}
+
+const onDeleteBtnClick = (event) => {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todosArray));
+}
+
+const onTodoFormSubmit = (event) => {
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todosArray.push(newTodo);
+    todoInput.value = "";
+    paintTodo(newTodo);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", onTodoFormSubmit);
